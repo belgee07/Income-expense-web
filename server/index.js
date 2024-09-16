0f8f9cd0a7e1c7bf140ed38ee2083ec72952d4fe
belgee7 <belge0722@gmail.com>
2024-09-16 12:30:26 +0800

    hiilee

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log("gg");
+import cors from "cors";
+import env from "dotenv";
+import express from "express";
+
+env.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, console.log(`http://localhost:${port}`));
