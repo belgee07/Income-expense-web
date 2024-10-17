3e8db289db472b3bcd01e2da3dc852ed53f83656
belgee7 <belge0722@gmail.com>
2024-10-17 10:35:25 +0800

    aa

--- client/src/app/dashboard/page.jsx
+++ client/src/app/dashboard/page.jsx
@@ -2,8 +2,8 @@ import { Dashboard } from "@/components";
 
 export default function page() {
   return (
-    <div>
+    <>
       <Dashboard />
-    </div>
+    </>
   );
 }
--- client/src/app/records/page.jsx
+++ client/src/app/records/page.jsx
@@ -1,11 +1,10 @@
 import { RecordLeft } from "@/components";
 import React from "react";
 
-const page = () => {
+export default function page() {
   return (
     <>
       <RecordLeft />
     </>
   );
-};
-export default page;
+}
